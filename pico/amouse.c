e1c200c564862da1a2cda56fe30c35d712ade68d
Aviancer <80006672+Aviancer@users.noreply.github.com>
2021-12-02 13:17:00 +0200

    Pico: WIP: Rebuild entire tinyusb and HID handling due to changes in library.

--- pico/amouse.c
+++ pico/amouse.c
@@ -43,6 +43,15 @@ void set_opts(struct opts *options) {
   options->wheel = 1;
 }
 
+// NEW
+#define MAX_HID_REPORT  4
+// Each HID instance can has multiple reports
+static struct
+{
+  uint8_t report_count;
+  tuh_hid_report_info_t report_info[MAX_HID_REPORT];
+}hid_info[CFG_TUH_HID];
+
 
 /*** Global state variables ****/
 
@@ -57,6 +66,7 @@ static uint32_t txtimer_target; // Serial transmit timer target time
 // Aggregate movements before sending
 CFG_TUSB_MEM_SECTION static hid_mouse_report_t usb_mouse_report;
 CFG_TUSB_MEM_SECTION static hid_mouse_report_t usb_mouse_report_prev;
+CFG_TUSB_MEM_SECTION static char serial_in_buffer[64] = { 0 }; //NEW
 
 // DEBUG
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
@@ -75,13 +85,161 @@ void queue_tx(mouse_state_t *mouse) {
 
 /*** USB comms ***/
 
-void tuh_hid_mouse_mounted_cb(uint8_t dev_addr) {
-  // printf("A Mouse device (address %d) is mounted\r\n", dev_addr);
+//static void process_mouse_report(hid_mouse_report_t const * report);
+static inline void process_mouse_report(mouse_state_t *mouse, hid_mouse_report_t const *p_report);
+static void process_generic_report(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len);
+
+// invoked ISR context
+void tuh_cdc_xfer_isr(uint8_t dev_addr, xfer_result_t event, cdc_pipeid_t pipe_id, uint32_t xferred_bytes)
+{
+  (void) event;
+  (void) pipe_id;
+  (void) xferred_bytes;
+
+  printf(serial_in_buffer);
+  tu_memclr(serial_in_buffer, sizeof(serial_in_buffer));
+
+  tuh_cdc_receive(dev_addr, serial_in_buffer, sizeof(serial_in_buffer), true); // waiting for next data
 }
 
-void tuh_hid_mouse_unmounted_cb(uint8_t dev_addr) {
-  // printf("A Mouse device (address %d) is unmounted\r\n", dev_addr);
+// Invoked when device with hid interface is mounted
+// Report descriptor is also available for use. tuh_hid_parse_report_descriptor()
+// can be used to parse common/simple enough descriptor.
+// Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE, it will be skipped
+// therefore report_desc = NULL, desc_len = 0
+void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len)
+{
+  printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
+
+  // Interface protocol (hid_interface_protocol_enum_t)
+  const char* protocol_str[] = { "None", "Keyboard", "Mouse" };
+  uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
+
+  printf("HID Interface Protocol = %s\r\n", protocol_str[itf_protocol]);
+
+  // By default host stack will use activate boot protocol on supported interface.
+  // Therefore for this simple example, we only need to parse generic report descriptor (with built-in parser)
+  if ( itf_protocol == HID_ITF_PROTOCOL_NONE )
+  {
+    hid_info[instance].report_count = tuh_hid_parse_report_descriptor(hid_info[instance].report_info, MAX_HID_REPORT, desc_report, desc_len);
+    printf("HID has %u reports \r\n", hid_info[instance].report_count);
+  }
+
+  // request to receive report
+  // tuh_hid_report_received_cb() will be invoked when report is available
+  if ( !tuh_hid_receive_report(dev_addr, instance) )
+  {
+    printf("Error: cannot request to receive report\r\n");
+  }
 }
+
+// Invoked when device with hid interface is un-mounted
+void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
+  printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+}
+
+// Invoked when received report from device via interrupt endpoint
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
+{
+  uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
+
+  switch (itf_protocol)
+  {
+    case HID_ITF_PROTOCOL_KEYBOARD:
+      TU_LOG2("HID receive boot keyboard report\r\n");
+      //process_kbd_report( (hid_keyboard_report_t const*) report );
+    break;
+
+    case HID_ITF_PROTOCOL_MOUSE:
+      TU_LOG2("HID receive boot mouse report\r\n");
+      process_mouse_report(&mouse, (hid_mouse_report_t const*) report );
+    break;
+
+    default:
+      TU_LOG2("HID receive generic report\r\n");
+      // Generic report requires matching ReportID and contents with previous parsed report info
+      process_generic_report(dev_addr, instance, report, len);
+    break;
+  }
+
+  // continue to request to receive report
+  if ( !tuh_hid_receive_report(dev_addr, instance) )
+  {
+    printf("Error: cannot request to receive report\r\n");
+  }
+}
+
+//--------------------------------------------------------------------+
+// Generic Report
+//--------------------------------------------------------------------+
+static void process_generic_report(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
+{
+  (void) dev_addr;
+
+  uint8_t const rpt_count = hid_info[instance].report_count;
+  tuh_hid_report_info_t* rpt_info_arr = hid_info[instance].report_info;
+  tuh_hid_report_info_t* rpt_info = NULL;
+
+  if ( rpt_count == 1 && rpt_info_arr[0].report_id == 0)
+  {
+    // Simple report without report ID as 1st byte
+    rpt_info = &rpt_info_arr[0];
+  }else
+  {
+    // Composite report, 1st byte is report ID, data starts from 2nd byte
+    uint8_t const rpt_id = report[0];
+
+    // Find report id in the arrray
+    for(uint8_t i=0; i<rpt_count; i++)
+    {
+      if (rpt_id == rpt_info_arr[i].report_id )
+      {
+        rpt_info = &rpt_info_arr[i];
+        break;
+      }
+    }
+
+    report++;
+    len--;
+  }
+
+  if (!rpt_info)
+  {
+    printf("Couldn't find the report info for this report !\r\n");
+    return;
+  }
+
+  // For complete list of Usage Page & Usage checkout src/class/hid/hid.h. For examples:
+  // - Keyboard                     : Desktop, Keyboard
+  // - Mouse                        : Desktop, Mouse
+  // - Gamepad                      : Desktop, Gamepad
+  // - Consumer Control (Media Key) : Consumer, Consumer Control
+  // - System Control (Power key)   : Desktop, System Control
+  // - Generic (vendor)             : 0xFFxx, xx
+  if ( rpt_info->usage_page == HID_USAGE_PAGE_DESKTOP )
+  {
+    switch (rpt_info->usage)
+    {
+      case HID_USAGE_DESKTOP_KEYBOARD:
+        TU_LOG1("HID receive keyboard report\r\n");
+        // Assume keyboard follow boot report layout
+        //process_kbd_report( (hid_keyboard_report_t const*) report );
+      break;
+
+      case HID_USAGE_DESKTOP_MOUSE:
+        TU_LOG1("HID receive mouse report\r\n");
+        // Assume mouse follow boot report layout
+        process_mouse_report(&mouse, (hid_mouse_report_t const*) report );
+      break;
+
+      default: break;
+    }
+  }
+}
+
+
+
 
 int test_mouse_button(uint8_t buttons_state, uint8_t button) {
   if(buttons_state & button) { return 1; }
@@ -125,24 +283,6 @@ static inline void process_mouse_report(mouse_state_t *mouse, hid_mouse_report_t
 
   // Update previous mouse state
   usb_mouse_report_prev = *p_report;
-}
-
-// invoked ISR context
-void tuh_hid_mouse_isr(uint8_t dev_addr, xfer_result_t event) {
-  (void) dev_addr;
-  (void) event;
-}
-
-// Process USB HID events.
-void hid_task(void) {
-  uint8_t const addr = 1;
-
-  if(tuh_hid_mouse_is_mounted(addr)) {
-    if(!tuh_hid_mouse_is_busy(addr)) {
-      tuh_hid_mouse_get_report(addr, &usb_mouse_report);
-      process_mouse_report(&mouse, &usb_mouse_report);
-    }
-  }
 }
 
 
@@ -193,7 +333,7 @@ int main() {
       gpio_put(LED_PIN, true);
 
       tuh_task(); // tinyusb host task
-      hid_task(); // hid/mouse handling
+      //hid_task(); // hid/mouse handling
  
       if(time_reached(txtimer_target) || mouse.force_update) {
         runtime_settings(&mouse);
